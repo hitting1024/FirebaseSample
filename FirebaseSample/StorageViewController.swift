15103a8d58016b571314c591e40e78945d75181a
IOKA <hit0apps@gmail.com>
2017-05-13 22:50:02 +0900

    load file from firebase

--- FirebaseSample/StorageViewController.swift
+++ FirebaseSample/StorageViewController.swift
@@ -11,6 +11,8 @@ import FirebaseStorage
 
 class StorageViewController: UIViewController {
     
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
     fileprivate var storageRef: FIRStorageReference!
 
     override func viewDidLoad() {
@@ -20,6 +22,30 @@ class StorageViewController: UIViewController {
         let dict = NSDictionary(contentsOfFile: filePath)!
         let firebaseStorageBucket = dict["STORAGE_BUCKET"] as! String
         self.storageRef = FIRStorage.storage().reference(forURL: "gs://\(firebaseStorageBucket)")
+        
+        self.loadImage1()
+        self.loadImage2()
+    }
+    
+    fileprivate func loadImage1() {
+        self.loadImage("1", completion: { image in
+            self.image1.image = image
+        })
+    }
+    
+    fileprivate func loadImage2() {
+        self.loadImage("2", completion: { image in
+            self.image2.image = image
+        })
+    }
+    
+    fileprivate func loadImage(_ name: String, completion: @escaping (UIImage?)->Void) {
+        self.storageRef.child("\(name).png").data(withMaxSize: 1 * 1024 * 1024 /* 1MB */, completion: { data, error in
+            if error != nil {
+                return
+            }
+            completion(UIImage(data: data!))
+        })
     }
     
     @IBAction func uploadImage1() {
