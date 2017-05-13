85bc9351b4bae954359b5e4ee6c856cc4e671100
IOKA <hit0apps@gmail.com>
2017-05-13 18:13:12 +0900

    convert swift

--- FirebaseSample/ViewController.swift
+++ FirebaseSample/ViewController.swift
@@ -15,22 +15,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var addressField: UITextField!
     
-    private var databaseRef: FIRDatabaseReference!
-    private var databaseHandle: FIRDatabaseHandle!
+    fileprivate var databaseRef: FIRDatabaseReference!
+    fileprivate var databaseHandle: FIRDatabaseHandle!
     
-    private var users: [FIRDataSnapshot] = []
+    fileprivate var users: [FIRDataSnapshot] = []
     
     deinit {
-        self.databaseRef.child("users").removeObserverWithHandle(self.databaseHandle)
+        self.databaseRef.child("users").removeObserver(withHandle: self.databaseHandle)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.databaseRef = FIRDatabase.database().reference()
-        self.databaseHandle = self.databaseRef.child("users").observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+        self.databaseHandle = self.databaseRef.child("users").observe(.childAdded, with: { (snapshot) -> Void in
             self.users.append(snapshot)
-            self.table.insertRowsAtIndexPaths([NSIndexPath(forRow: self.users.count-1, inSection: 0)], withRowAnimation: .Automatic)
+            self.table.insertRows(at: [IndexPath(row: self.users.count-1, section: 0)], with: .automatic)
         })
     }
 
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func save() {
-        guard let name = self.nameField.text, address = self.addressField.text else {
+        guard let name = self.nameField.text, let address = self.addressField.text else {
             return
         }
         var data = ["name": name]
@@ -54,18 +54,18 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.users.count
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell! = table.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell! = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         let userSnapshot: FIRDataSnapshot! = self.users[indexPath.row]
         let user = userSnapshot.value as! Dictionary<String, String>
         let name = user["name"] as String!
         let address = user["address"] as String!
-        cell.textLabel?.text = name + ": " + address
+        cell.textLabel?.text = name! + ": " + address!
 
         return cell
     }
